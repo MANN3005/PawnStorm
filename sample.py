b2cbf9d5d0235c10792b2491a4e976804d91f003
Mann <mn906219@dal.ca>
2024-08-17 18:25:22 -0300

    added user input

--- sample.py
+++ sample.py
@@ -1,21 +1,19 @@
 class engine:
-    # this will contain 2 things :- 1) boolean whoseTurn 2) board
-    pass
     def __init__(self):
-        self.turn = 0
+        self.turn = 0  # 0 for White, 1 for Black
         self.board = []
-        l = ['r','n','b','q','k','b','n','r']
+        l = ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r']
         self.board.append(l)
-        l = ['p','p','p','p','p','p','p','p']
+        l = ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p']
         self.board.append(l)
         for i in range(4):
-            l2 = [' ',' ',' ',' ',' ',' ',' ',' ']
+            l2 = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
             self.board.append(l2)
-        l = ['P','P','P','P','P','P','P','P']
+        l = ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P']
         self.board.append(l)
-        l = ['R','N','B','Q','K','B','N','R']
+        l = ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
         self.board.append(l)
-    
+
     def get_board(self):
         return self.board
 
@@ -23,34 +21,46 @@ class engine:
         arr = self.board
         print("+---+---+---+---+---+---+---+---+")
         for i in range(8):
-            # print(arr[i])
             for j in range(8):
-                print( "| "+str(arr[i][j]),end=" ")
-            print("| "+str(8-i))
+                print("| " + str(arr[i][j]), end=" ")
+            print("| " + str(8 - i))
             print("+---+---+---+---+---+---+---+---+")
         print("  a   b   c   d   e   f   g   h")
 
-
     def whose_turn(self):
         if self.turn:
-            print("Black")
+            print("Black's turn")
         else:
-            print("White")
-    def make_moves(self, arr):
-        new_board = [row[:] for row in self.board]
-        for l in arr:
-            a = ord(l[0])-ord('a')
-            b = ord(l[1])-ord('0')
-            c = ord(l[2])-ord('a')
-            d = ord(l[3])-ord('0')
-            new_board[8-d][c] = new_board[8-b][a]
-            new_board[8-b][a] = " "
-        
-        self.board = new_board
+            print("White's turn")
 
+    def make_move(self, move):
+        a = ord(move[0]) - ord('a')
+        b = ord(move[1]) - ord('1')
+        c = ord(move[2]) - ord('a')
+        d = ord(move[3]) - ord('1')
+        self.board[7 - d][c] = self.board[7 - b][a]
+        self.board[7 - b][a] = " "
+        self.turn = 1 - self.turn  # Toggle turn between 0 (White) and 1 (Black)
 
+   
+    def _convert_move(self, start_col, start_row, end_col, end_row):
+        return chr(start_col + ord('a')) + str(8 - start_row) + chr(end_col + ord('a')) + str(8 - end_row)
 
+    def play(self):
+        self.get_board_visual()
+        while True:
+            self.whose_turn()
+            print("Top moves:", self.get_top_move())
+            move = input("Enter your move (e.g., e2e4): ")
+            if move == "quit":
+                print("Game ended.")
+                break
+            if len(move) == 4 and move[0] in 'abcdefgh' and move[2] in 'abcdefgh' and move[1] in '12345678' and move[3] in '12345678':
+                self.make_move(move)
+                self.get_board_visual()
+            else:
+                print("Invalid move. Please enter a valid move.")
+
+# To start the game:
 az = engine()
-az.get_board_visual()
-az.make_moves(["e2e4","e7e5"])
-az.get_board_visual()
+az.play()
